4d1a7225fb550773e9d12cb690b9e5abe6fb8591
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-12 10:46:00 +0100

    Update PopupModal.js
    Added ability to confirm/cancel using enter and escape keys

--- chore-champion/src/components/PopupModal.js
+++ chore-champion/src/components/PopupModal.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const PopupModal = ({ message, onConfirm, onCancel }) => {
+    const modalRef = useRef(null);
+
+    useEffect(() => {
+        if (modalRef.current) {
+            modalRef.current.focus();
+        }
+    } ,[]);
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            onConfirm();
+        } else if (event.key === "Escape") {
+            onCancel();
+        }
+    };
+
     return (
         <div className="modal-overlay">
-            <div className="popup-modal">
-            <p>{message}</p>
+            <div className="popup-modal" onKeyDown={handleKeyDown} tabIndex="0" ref={modalRef}>
+                <p>{message}</p>
 
-            <div>
-                <button onClick={onConfirm}>Confirm</button>
-                <button onClick={onCancel}>Cancel</button>
+                <div>
+                    <button onClick={onConfirm}>Confirm</button>
+                    <button onClick={onCancel}>Cancel</button>
+                </div>
             </div>
         </div>
-        </div>
-
     );
 }
 
